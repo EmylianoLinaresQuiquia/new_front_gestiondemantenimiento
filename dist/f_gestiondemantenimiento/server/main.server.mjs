368cea13f416093e3b603bdb98c5e5850d0679ca
lolielperoo777 <emilianolinaresquiquia@gmail.com>
2024-08-24 12:19:22 -0500

    update config

---
+++ dist/f_gestiondemantenimiento/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-K4MXWKEE.mjs";import"./chunk-T3X6UWQR.mjs";import"./chunk-3DEPWTXM.mjs";import"./chunk-NKJ3RCQH.mjs";import"./chunk-ZEHOJ5DW.mjs";import"./chunk-NNB2SIY7.mjs";import"./chunk-HVFRRH3V.mjs";import"./chunk-7I42D7FU.mjs";export{a as default};
