5a6f268c7c24b9be981c9e0fafa35854538adaf3
lolielperoo777 <emilianolinaresquiquia@gmail.com>
2024-08-24 10:43:08 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Servir archivos estáticos desde el directorio 'dist'
-app.use(express.static(path.join(__dirname, 'dist', 'f_gestiondemantenimiento')));
-
-// Redirigir todas las rutas al index.html de Angular
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'f_gestiondemantenimiento', 'index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Escuchar en el puerto definido por Heroku
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+app.listen(process.env.PORT || 4200);
