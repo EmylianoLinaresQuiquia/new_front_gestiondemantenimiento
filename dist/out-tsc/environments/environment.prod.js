974c650d53d17c7452e1d60e3197cff177075939
lolielperoo777 <emilianolinaresquiquia@gmail.com>
2024-08-24 23:52:28 -0500

    tengo sueño

--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api/'
-    //https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api/
-    //https://localhost:7242/api/
-};
-//# sourceMappingURL=environment.prod.js.map
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.ts',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'babel-loader',
+          '@ngtools/webpack'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+    runtimeChunk: 'single',
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-report.html',
+      openAnalyzer: false
+    })
+  ]
+};
