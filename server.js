af8dde4eefa936021f30074e847d2e201f063d1e
lolielperoo777 <emilianolinaresquiquia@gmail.com>
2024-08-25 05:11:05 -0500

    nose

--- server.js
+++ server.js
@@ -2,19 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Ruta de la carpeta de distribución de tu aplicación Angular
-const distFolder = path.join(__dirname, 'dist/f-gestiondemantenimiento');
+app.use(express.static(path.join(__dirname, 'dist/f-gestiondemantenimiento')));
 
-// Servir archivos estáticos desde la carpeta de distribución
-app.use(express.static(distFolder));
-
-// Manejar todas las solicitudes y servir el archivo index.html
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(distFolder, 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/f-gestiondemantenimiento', 'index.html'));
 });
 
-// Configurar el puerto para Heroku o un puerto predeterminado
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Servidor Express escuchando en http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
