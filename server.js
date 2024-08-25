babbc0ea90a26d683d87982fca8b78bc618264f0
lolielperoo777 <emilianolinaresquiquia@gmail.com>
2024-08-25 07:56:14 -0500

    bill

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-const path = require('path');
+import path from 'path';
 const { CommonEngine } = require('@angular/ssr');
 const { APP_BASE_HREF } = require('@angular/common');
 const { mainServer } = require('./src/main.server');
