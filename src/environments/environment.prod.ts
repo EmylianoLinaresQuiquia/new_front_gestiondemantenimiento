5ef0867a1bfa955d4d657378470dc5b569ebef04
EmylianoLinaresQuiquia <emilianolinaresquiquia@gmail.com>
2025-08-24 15:34:59 +0000

    update apiurl environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api'
+	apiUrl: 'https://net8-web-api-production-a6e5.up.railway.app/api'
 	//https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api
 	//https://localhost:7242/api
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api'
+  apiUrl: 'https://net8-web-api-production-a6e5.up.railway.app/api'
   //https://localhost:7242/api
   //https://apigestiondemantenimiento2024-ba317c3bc05b.herokuapp.com/api
 };
