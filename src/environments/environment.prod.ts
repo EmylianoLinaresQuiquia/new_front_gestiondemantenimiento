9479f28f7efe8a3701e735eea8c104ee20d22488
EmylianoLinaresQuiquia <emilianolinaresquiquia@gmail.com>
2025-08-27 05:59:43 +0000

    update apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['NG_APP_API_URL'] || 'https://net8-web-api-production-a6e5.up.railway.app/api'
+  apiUrl: 'https://net8-web-api-production-a6e5.up.railway.app/api'
 };
